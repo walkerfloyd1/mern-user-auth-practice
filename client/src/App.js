2c620f03c7e414b2df1758f72c2caaacdc4849e8
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-09 16:12:04 -0700

    routing in app.js for login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Navbar from "./components/layout/Navbar";
 import Landing from './components/layout/Landing';
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
     </div>
+    </Router>
   );
 }
 

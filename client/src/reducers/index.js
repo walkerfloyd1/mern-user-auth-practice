2b27227cdfc39c3382aee11f0dc7ac95f556c3ba
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-09 16:45:07 -0700

    fixed combined reducers problem

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,8 @@
-import { combinedReducers } from "redux";
+import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 
-export default combinedReducers({
+export default combineReducers({
     auth: authReducer,
     errors: errorReducer
 })

6adbbf2730ec829adc017148ca7faf6216417476
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-09 16:43:40 -0700

    finished with front end and redux set up

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
+
+export const registerUser = (userData, history) => dispatch => {
+    axios.post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    })
+    );
+};
+
+export const loginUser = userData => dispatch => {
+    axios.post("/api/users/login", userData)
+    .then(res => {
+        const { token } = res.data;
+        setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => 
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    );
+};
+
+export const setUserLoading = () => {
+    return {
+        type: USER_LOADING
+    };
+};
+
+export const logoutUser = () => dispatch => {
+    localStorage.removeItem("jwtToken");
+    setAuthToken(false);
+    dispatch(setCurrentUser({}));
+};

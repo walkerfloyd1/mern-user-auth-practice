47a1150152855c3dc005c1c852b760751591e76f
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-09 16:19:41 -0700

    created redux store

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,15 @@ import Login from "./components/auth/Login";
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
+import { Provider } from "react-redux";
+
+import store from './store';
+
 import './App.css';
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
     <div className="App">
       <Navbar />
@@ -18,6 +23,7 @@ function App() {
       <Route exact path="/login" component={Login} />
     </div>
     </Router>
+    </Provider>
   );
 }
 

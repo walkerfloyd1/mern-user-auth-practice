d79a6916e3a214ebf9bfe98e89b644fc10e56ccc
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-12 14:48:35 -0700

    updated app.js to check for token

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,34 @@ import Navbar from "./components/layout/Navbar";
 import Landing from './components/layout/Landing';
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from './components/private-route/PrivateRoute';
+import Dashboard from './components/dashboard/Dashboard';
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import jwt_decode from "jwt-decode";
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
 import { Provider } from "react-redux";
 
 import store from './store';
 
 import './App.css';
+
+if (localStorage.jwtToken) {
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+
+  const decoded = jwt_decode(token);
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now();
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser())
+
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
@@ -21,6 +41,9 @@ function App() {
       <Route exact path="/" component={Landing} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/login" component={Login} />
+      <Switch>
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      </Switch>
     </div>
     </Router>
     </Provider>
